a385734969ac1bebce4f5545c5e13105d68b3f8b
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-18 04:58:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = [" O  ", "   ", "   ", "   ", " X  ", "   ", "   ", "   ", "   ",]
 def display_board(board)
-  puts " O |   |   "
+  puts "   |   |   "
   puts "-----------"
-  puts "   | X |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
