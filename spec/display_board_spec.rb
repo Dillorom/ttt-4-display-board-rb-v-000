88eb188ac56e6780d8792229a7bafc6b4aec4642
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-18 05:39:09 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -165,7 +165,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-    
+
 
       expect(rows[0]).to eq("   |   |   ")
       expect(rows[1]).to eq("-----------")
@@ -183,7 +183,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # entirely filled with Os?
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
-      
+
       expect(rows[0]).to eq("   |   |   ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq("   |   |   ")
